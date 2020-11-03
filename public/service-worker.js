dfdb9e34c1245f6e1a458c861c070e8322cc74f6
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-11-03 18:15:54 -0500

    removed some unnecessary code

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,18 +11,6 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// const iconSizes = ["192", "512"];
-// const iconFiles = iconSizes.map(
-//     (size) => `/assets/images/icons/icon-${size}x${size}.png`
-// );
-
-// const staticFilesToPreCache = [
-//     "/",
-//     "/index.js",
-//     "/manifest.webmanifest",
-// ].concat(iconFiles);
-
-
 // install
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
